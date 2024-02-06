5ce37829a121b8be2c104c5e7058959a2e9e7f33
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-02-06 13:16:26 +0700

    updaate upload multiple files

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -1,20 +1,28 @@
-import {
-  Controller,
-  Post,
-  UseInterceptors,
-  UploadedFile,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UseInterceptors, UploadedFile, UploadedFiles } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { FileUploadService } from './file-upload.service';
 import { BufferedFile } from 'src/minio-client/file.model';
 
 @Controller('file-upload')
 export class FileUploadController {
-  constructor(private fileUploadService: FileUploadService) {}
+  constructor(
+    private fileUploadService: FileUploadService
+  ) {}
 
   @Post('single')
   @UseInterceptors(FileInterceptor('image'))
-  async uploadSingle(@UploadedFile() image: BufferedFile) {
-    return await this.fileUploadService.uploadSingle(image);
+  async uploadSingle(
+    @UploadedFile() image: BufferedFile
+  ) {
+    return await this.fileUploadService.uploadSingle(image)
+  }
+
+  @Post('many')
+  // @UseInterceptors(FileFieldsInterceptor('image'))
+  @UseInterceptors(FilesInterceptor('image'))
+  async uploadMany(
+    @UploadedFiles() files: Array<BufferedFile>,
+  ) {
+    return this.fileUploadService.uploadMany(files)
   }
 }
