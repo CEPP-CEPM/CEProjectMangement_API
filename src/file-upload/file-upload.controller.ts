c6c65d8b74a4e397b00b0673e7ff2a5344c7080c
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-02-10 14:05:37 +0700

    update role guard

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -3,6 +3,9 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { FileUploadService } from './file-upload.service';
 import { BufferedFile } from 'src/minio-client/file.model';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { RolesGuard } from 'src/auth/guards/role.guard';
 
 @Controller('file-upload')
 export class FileUploadController {
@@ -11,7 +14,8 @@ export class FileUploadController {
   ) {}
 
   @Post('single')
-  @UseGuards(JwtAuthGuard)
+  @Roles(Role.PROCTOR)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @UseInterceptors(FileInterceptor('image'))
   async uploadSingle(
     @UploadedFile() image: BufferedFile
