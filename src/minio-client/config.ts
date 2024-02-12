af69905dde943f876b5d492303b045f64eecb7de
Sobitora <web_124@hotmail.com>
2024-02-12 20:19:19 +0700

    update prisma

--- src/minio-client/config.ts
+++ src/minio-client/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     MINIO_ENDPOINT: 'localhost',
     MINIO_PORT: 9000,
-    MINIO_ACCESSKEY: 'BMv3anSVGSVnIZcH8GGz',
-    MINIO_SECRETKEY: 'IKQt6NzzknpXKtCga043hsU4XaXP63a8lj50NXzc',
+    MINIO_ACCESSKEY: 'zEoImvEv1CzUTPQ0dDxM',
+    MINIO_SECRETKEY: '92bMHb0l5HaaBzP6TBLbxzsUUQ4OKqhHMIdetUc7',
     MINIO_BUCKET: 'cepm'
 }
