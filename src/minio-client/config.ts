257129a685121e2a2b3cc9764581beda23fd8fa7
Web <web_124@hotmail.com>
2024-02-24 22:14:07 +0700

    bucket

--- src/minio-client/config.ts
+++ src/minio-client/config.ts
@@ -3,5 +3,5 @@ export const config = {
     MINIO_PORT: 9000,
     MINIO_ACCESSKEY: 'KPpkftWBul0YQJParpTl',
     MINIO_SECRETKEY: 'cACrB8c3EJtvMtEmlifKczjbsPxYIyRq7o9HppYO',
-    // MINIO_BUCKET: 'cepm'
+    MINIO_BUCKET: 'cepm'
 }
