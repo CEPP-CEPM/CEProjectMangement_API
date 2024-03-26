a78f8034fb0ee85fe4d6bcb8c61c4a8b01b599b6
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2024-03-27 01:54:00 +0700

    disable swaggerUI

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('CE Projectmanagement')
-    .setDescription('CE Projectmanagement API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('CE Projectmanagement')
+  //   .setDescription('CE Projectmanagement API description')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   await app.listen(5000);
